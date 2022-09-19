4b79f60f7e421763b8f3fd0775a53b27c971b5e6
the-best-is-best <michelle.raouf@outlook.com>
2022-09-19 16:10:42 +0200

    bloc handled

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
 import 'package:booking_app/app/app.dart';
 import 'package:booking_app/app/di.dart';
-import 'package:booking_app/app/network/failure.dart';
-import 'package:booking_app/features/auth/domain/user_model.dart';
-import 'package:booking_app/features/auth/register/repository_register.dart';
-import 'package:dartz/dartz.dart' as z;
 import 'package:flutter/material.dart';
-import 'package:mit_x/mit_x.dart';
 
 void main() async {
   await initApp();
-  // z.Either<Failure, UserModel> response = await di<RepositoryRegister>()
-  //     .register(ResterRequests(
-  //         email: "test@testing.test",
-  //         name: "testing12",
-  //         password: "123456",
-  //         passwordConfirm: "123456"));
-  // response.fold(
-  //   (l) => print(l.messages),
-  //   (r) => print(r),
-  // );
   runApp(const MyApp());
 }
