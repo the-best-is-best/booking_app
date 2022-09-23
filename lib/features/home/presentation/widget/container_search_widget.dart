2a64ba42317a57782c78ace8e80aa952c1bbe205
the-best-is-best <michelle.raouf@outlook.com>
2022-09-23 20:58:02 +0200

    search home open explore

--- lib/features/home/presentation/widget/container_search_widget.dart
+++ lib/features/home/presentation/widget/container_search_widget.dart
@@ -1,6 +1,8 @@
 import 'package:booking_app/core/utils/color_manager.dart';
+import 'package:booking_app/core/utils/routes_manager.dart';
 import 'package:booking_app/core/utils/values_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:mit_x/mit_x.dart';
 
 class ContainerSearchWidget extends StatelessWidget {
   const ContainerSearchWidget({Key? key}) : super(key: key);
@@ -20,24 +22,29 @@ class ContainerSearchWidget extends StatelessWidget {
           color: ColorManager.darkGrey,
           borderRadius: BorderRadius.circular(AppSize.s20),
         ),
-        child: Row(
-          children: [
-            const Icon(
-              Icons.search,
-              color: ColorManager.primary,
-              size: AppSize.s25,
-            ),
-            SizedBox(
-              width: size.width * 0.02,
-            ),
-            Text(
-              'Where are you going ?',
-              style: Theme.of(context)
-                  .textTheme
-                  .displayMedium!
-                  .copyWith(color: ColorManager.grey),
-            ),
-          ],
+        child: InkWell(
+          onTap: () {
+            MitX.toNamed(Routes.exploreScreen);
+          },
+          child: Row(
+            children: [
+              const Icon(
+                Icons.search,
+                color: ColorManager.primary,
+                size: AppSize.s25,
+              ),
+              SizedBox(
+                width: size.width * 0.02,
+              ),
+              Text(
+                'Where are you going ?',
+                style: Theme.of(context)
+                    .textTheme
+                    .displayMedium!
+                    .copyWith(color: ColorManager.grey),
+              ),
+            ],
+          ),
         ),
       ),
     );
